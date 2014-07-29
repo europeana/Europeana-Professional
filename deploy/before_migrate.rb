1829070989cf1e93939707116b49ef7ab454d16a
tobias2k <tobias@twokings.nl>
2014-07-29 15:31:20 +0200

    Added a hook to automatically generate config_local.yml

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,21 @@
+# as per:
+# https://support.cloud.engineyard.com/entries/21016568-use-deploy-hooks
+
+require 'yaml'
+
+config_local = {
+    database: {
+        driver: 'mysql',
+        databasename: ENV['DB_NAME'],
+        username: ENV['DB_USER'],
+        password: ENV['DB_PASS']
+    }
+}
+
+config_filename = "#{config.release_path}/app/config/config_local.yml"
+
+yaml_source = YAML.dump(config_local).split("\n").drop(1).join("\n")
+
+File.open(config_filename, "w") do |f|
+    f.write(yaml_source)
+end
