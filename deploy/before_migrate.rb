9f4bda490839b7de6068e67241e8160616eff8d7
tobias2k <tobias@twokings.nl>
2014-08-06 11:08:31 +0200

    Let's try again; different approach

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,20 +3,27 @@
 
 require 'yaml'
 
-config_local = {
-    'database' => {
-        'driver' => 'mysql',
-        'databasename' => ENV['DB_NAME'],
-        'username' => ENV['DB_USER'],
-        'password' => ENV['DB_PASS'],
-        'pompidom' => ENV['HOME']
+# Load database configuration
+ey_db_configs = YAML.load("#{config.shared_path}/config/database.yml")
+ey_db_config = ey_db_config[config.environment_name]
+if ey_db_config === Nil then
+    warning("Database configuration for '#{config.environment_name}' not found!")
+end
+
+# Build Bolt config_local data structure
+my_db_config = {
+    "database" => {
+        "driver" => ey_db_config['adapter'],
+        "host" => ey_db_config['host'],
+        "databasename" => ey_db_config['database'],
+        "username" => ey_db_config['username'],
+        "password" => ey_db_config['password']
     }
 }
 
+# Write Bolt config_local YAML file
 config_filename = "#{config.release_path}/app/config/config_local.yml"
-
-yaml_source = YAML.dump(config_local).split("\n").drop(1).join("\n")
-
 File.open(config_filename, "w") do |f|
     f.write(yaml_source)
 end
+
