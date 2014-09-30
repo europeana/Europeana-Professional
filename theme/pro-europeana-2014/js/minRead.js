0840147c26c186801c9c76dbe8a58156bb5a4d5a
patgit <pat@tetten.com>
2014-09-30 14:50:36 +0200

    - bugfix

--- theme/pro-europeana-2014/js/minRead.js
+++ theme/pro-europeana-2014/js/minRead.js
@@ -5,36 +5,39 @@
  * 	+ set label configurable
  * 	+ add else-clause to settings.archive to avoid text flickering
 */
+
 (function($) {
-$.fn.minRead = function( options ) {
-var settings = $.extend({}, $.fn.minRead.defaults, options);
-return this.each( function() {
-var element = $(this);
-if (settings.archive) {
-	var articleLink = element.find(settings.anchor);
-	var articleUrl = articleLink.attr("href");
-	$.get(articleUrl, function(data){
-	data = data.replace(/<img.+>/gi, "");
-	var text = $(data).find(settings.archiveText).text();
-	var archiveTime = calculateTime(text, settings);
-	element.find(settings.where).text(archiveTime + settings.label);
-	});
+	
+	$.fn.minRead = function( options ) {
+		var settings = $.extend({}, $.fn.minRead.defaults, options);
+		return this.each( function() {
+			var element = $(this);
+			if (settings.archive) {
+				var articleLink = element.find(settings.anchor);
+				var articleUrl = articleLink.attr("href");
+				$.get(articleUrl, function(data){
+					var text = $(data).find(settings.archiveText).text();
+					var archiveTime = calculateTime(text, settings);
+					element.find(settings.where).text(archiveTime + settings.label);
+				});
+			}
+			else {
+				var time = calculateTime(element.text(), settings);
+				element.parent().find(settings.where).text(time + settings.label);
+			}
+		});
+	};
+	function calculateTime(text, settings) {
+	return Math.ceil(text.split(' ').length / settings.wordsPerMinute) || 1;
 	}
-else {
-	var time = calculateTime(element.text(), settings);
-	element.parent().find(settings.where).text(time + settings.label);
-}
-});
-};
-function calculateTime(text, settings) {
-return Math.ceil(text.split(' ').length / settings.wordsPerMinute) || 1;
-}
-$.fn.minRead.defaults = {
-where	: ".min-read",
-wordsPerMinute	: 180,
-archive	: false,
-archiveText	: ".text",
-anchor : ".article-link",
-label: " min read"
-};
+	
+	$.fn.minRead.defaults = {
+		where	: ".min-read",
+		wordsPerMinute	: 180,
+		archive	: false,
+		archiveText	: ".text",
+		anchor : ".article-link",
+		label: " min read"
+	};
+
 })(jQuery);
