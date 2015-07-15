eb500ebcb2edd376cb919452b40f60ac3a11dc65
Bob den Otter <bobdenotter@gmail.com>
2015-07-15 13:18:04 +0200

    Hardcoded redirect. See assembla #310

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
  * tasks needed to return the app.  Once we get the app, we simply tell it
  * to run, building a beautiful web page for you and other visitors.
  */
+ 
+ // Override hardcoded, because it can't be caught by Boltredirector..
+if ($_SERVER['HTTP_HOST'] == "www.europeana-libraries.eu") {
+    header("location: http://pro.europeana.eu/get-involved/projects/project-list/europeana-libraries");
+    die();
+}
 
 /**
  * @var \Bolt\Application $app
