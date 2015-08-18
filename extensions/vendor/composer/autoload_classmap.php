2915c5ca9b0b1b210692d7b840c2d848bb58d4eb
Bob den Otter <bob@twokings.nl>
2015-08-18 10:39:46 +0200

    Updating to bolt 2.2.8

--- extensions/vendor/composer/autoload_classmap.php
+++ extensions/vendor/composer/autoload_classmap.php
@@ -12,6 +12,7 @@ return array(
     'Bolt\\Extension\\Bolt\\RSSFeed\\Extension' => $vendorDir . '/bolt/rssfeed/Extension.php',
     'Bolt\\Extension\\Bolt\\Sitemap\\Extension' => $vendorDir . '/bolt/sitemap/Extension.php',
     'Bolt\\Extension\\Europeana\\FileHelper\\Extension' => $baseDir . '/local/europeana/file-helper/Extension.php',
+    'Bolt\\Extension\\Europeana\\FrontendExt\\Extension' => $baseDir . '/local/europeana/frontendext/Extension.php',
     'Bolt\\Extension\\Europeana\\GoogleSearch\\Extension' => $baseDir . '/local/europeana/google-search/Extension.php',
     'Bolt\\Extension\\Europeana\\StructureTree\\Extension' => $baseDir . '/local/europeana/structure-tree/Extension.php',
     'Bolt\\Extension\\FoundryCode\\Redirector' => $vendorDir . '/foundrycode/boltredirector/Redirector.php',
